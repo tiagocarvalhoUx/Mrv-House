01759f1f31c1c0083bea0fe7dbd6f5f710792c02
Tiago Carvalho <136613404+tiago-bozo@users.noreply.github.com>
2025-05-17 17:30:43 -0300

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
-import HouseContextProvider from "./components/HouseContext"
+import HouseContextProvider from "./components/HouseContext.jsx";
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root")).render(
   <HouseContextProvider>
     <Router>
       <StrictMode>
         <App />
       </StrictMode>
     </Router>
-
   </HouseContextProvider>
 );
